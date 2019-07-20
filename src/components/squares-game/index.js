ab267100146207afde94cf795966e1b74d3e7aeb
Roman Zhyliov <romanfromrome.com@gmail.com>
2019-07-20 21:46:40 +0800

    timers abd levels wohoo 🌈 (#2)

--- src/components/squares-game/index.js
+++ src/components/squares-game/index.js
@@ -11,16 +11,46 @@ function getIsBomb(probabilityFactor) {
   return !!Math.round(Math.max(0, Math.random()- probabilityFactor))
 }
 
+function getIsActive(probabilityFactor) {
+  return !!Math.round(Math.max(0, Math.random() - (0.5-probabilityFactor)))
+}
+
 export default function() {
   const dispatch = useDispatch()
   const totalSquares = useSelector(state => state.squareGame.totalSquares);
   const probabilityFactor = useSelector(state => state.squareGame.probabilityFactor)
 
-  console.log(probabilityFactor)
-
   const onExplode = (idx) => dispatch(actions.squareBlast(idx));
   const onActivate = (idx) => dispatch(actions.squareActivate(idx));
   const onDeactivate = (idx) => dispatch(actions.squareDeactivate(idx));
+
+  const Squares = () => <div className={
+    [
+        style.grid,
+        style.searchResultsContainer,
+    ].join(' ')
+}>
+    {
+      Array.from({length: totalSquares}, (v, i) => i).map((item,idx) => {
+        const isActive = getIsActive(probabilityFactor);
+        const isBomb = getIsBomb(probabilityFactor);
+        return (
+                // TODO: random active state calc
+                <Square
+                  idx={idx}
+                  isActive={isActive}
+                  isBomb={isBomb}
+                  onExplode={onExplode}
+                  onActivate={onActivate}
+                  onDeactivate={onDeactivate}
+                  probabilityFactor={probabilityFactor}
+                  getIsBomb={getIsBomb}
+                  {...item}
+                  key={idx}
+                />
+      )})
+    }
+</div>
 
   return (
     <div className={style.container}>
@@ -29,33 +59,7 @@ export default function() {
         <div
             className={style.resultsContainer}
         >
-            <div className={
-                [
-                    style.grid,
-                    style.searchResultsContainer,
-                ].join(' ')
-            }>
-                {
-                  Array.from({length: totalSquares}, (v, i) => i).map((item,idx) => {
-                    const isActive = !!Math.round(Math.random());
-                    const isBomb = getIsBomb(probabilityFactor);
-                    return (
-                            // TODO: random active state calc
-                            <Square
-                              idx={idx}
-                              isActive={isActive}
-                              isBomb={isBomb}
-                              onExplode={onExplode}
-                              onActivate={onActivate}
-                              onDeactivate={onDeactivate}
-                              probabilityFactor={probabilityFactor}
-                              getIsBomb={getIsBomb}
-                              {...item}
-                              key={idx}
-                            />
-                  )})
-                }
-            </div>
+          <Squares />
         </div>
       </div>
     </div>

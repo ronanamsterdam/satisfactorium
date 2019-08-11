b32fb3363919562bf1defa2129bee04e2fecb09d
Ronan Amsterdam <romanfromrome.com@gmail.com>
2019-08-11 07:26:09 +0100

    wip: device to hooks

--- src/components/squares-game/utils/device/index.js
+++ src/components/squares-game/utils/device/index.js
@@ -1,78 +1,64 @@
 // TODO: refactor to hooks
 
 // non ui component to gather and react to client device's specifics
-import  { Component } from 'react';
-// import {DEVICE_FORM_FACTORS}    from 'statics/strings/reducers/ux';
+import  { useEffect } from 'react';
 
-import { connect }              from 'react-redux';
-import { bindActionCreators }   from 'redux';
-import AppActions               from '../../../../actions';
+import { useDispatch } from "react-redux";
+import actions from '../../../../actions';
+// import {DEVICE_FORM_FACTORS}    from 'statics/strings/reducers/ux';
 
 const debounce = require('../../../../utils').debounceCl();
 // const coreStyleVars = require("!less-vars-loader?camelCase!style/variables.less");
 
-class Device extends Component {
-    componentWillMount() {
-        this.__setAgent();
-        this.__setDimensions();
-        this.__setListeners();
+export default function() {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+      __setAgent({dispatch});
+      __setDimensions({dispatch});
+      __setListeners();
+      return () => __removeListeners();
+    })
+
+    const __setListeners = () => window.addEventListener('resize', onResize)
+    const __removeListeners = () => window.removeEventListener('resize', onResize);
+
+    const onResize = (e) => debounce(() => __onResize({dispatch, e}), 300);
+    const __onResize = ({dispatch,e}) => __setDimensions({dispatch, e});
+
+    const __setAgent = ({dispatch}) => {
+      const agent = navigator.userAgent;
+      dispatch(actions.setDeviceUserAgent(agent));
     }
 
-    componentWillUnmount() {
-        this.__removeListeners();
+    const __setDimensions = ({dispatch, props}) => {
+      // const agent = navigator.userAgent;
+
+      const {innerHeight, innerWidth, outerWidth, outerHeight} = window;
+
+      // const tabletBreak = parseInt(coreStyleVars.tabletBreak);
+      // const mobileBreak = parseInt(coreStyleVars.mobileBreak);
+
+      // let factor = DEVICE_FORM_FACTORS.DESKTOP
+
+      // console.table({innerHeight, innerWidth});
+
+      // if (innerWidth <= mobileBreak) {
+      //     factor = DEVICE_FORM_FACTORS.MOBILE;
+      // } else if (innerWidth <= tabletBreak) {
+      //     factor = DEVICE_FORM_FACTORS.TABLET;
+      // }
+
+      // this.props.actions.setDeviceFormFactor(factor);
+
+      dispatch(actions.setDeviceDimensions({
+          height:         innerHeight,
+          width:          innerWidth,
+          outerHeight,
+          outerWidth,
+      }));
     }
 
-    __setListeners = () => {
-        window.addEventListener('resize', this.onResize)
-    }
-
-    __removeListeners = () => {
-        window.removeEventListener('resize', this.onResize);
-    }
-
-    onResize = e => debounce(()=>this.__onResize(e),300)
-
-    __onResize = e => this.__setDimensions(e)
-
-    __setAgent = () => {
-        const agent = navigator.userAgent;
-        this.props.actions.setDeviceUserAgent(agent);
-    }
-
-    __setDimensions = () => {
-        // const agent = navigator.userAgent;
-
-        const {innerHeight, innerWidth, outerWidth, outerHeight} = window;
-
-        // const tabletBreak = parseInt(coreStyleVars.tabletBreak);
-        // const mobileBreak = parseInt(coreStyleVars.mobileBreak);
-
-        // let factor = DEVICE_FORM_FACTORS.DESKTOP
-
-        // console.table({innerHeight, innerWidth});
-
-        // if (innerWidth <= mobileBreak) {
-        //     factor = DEVICE_FORM_FACTORS.MOBILE;
-        // } else if (innerWidth <= tabletBreak) {
-        //     factor = DEVICE_FORM_FACTORS.TABLET;
-        // }
-
-        // this.props.actions.setDeviceFormFactor(factor);
-
-        this.props.actions.setDeviceDimensions({
-            height:         innerHeight,
-            width:          innerWidth,
-            outerHeight,
-            outerWidth,
-        });
-    }
-
-    render = () => null
+    return null;
 }
-
-const mapDispatch = dispatch => ({
-    actions: bindActionCreators(AppActions, dispatch)
-});
-
-export default
-    connect(null, mapDispatch)(Device)

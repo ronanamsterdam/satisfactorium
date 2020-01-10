7613dfc07ee6349c2b956b980a062ff61373dcf6
Ronan Amsterdam <romanfromrome.com@gmail.com>
2020-01-10 17:30:25 +0800

    reafactoring styles

--- src/components/squares/sqaure/index.js
+++ src/components/squares/sqaure/index.js
@@ -1,53 +1,40 @@
 import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import style            from "./style.module.less";
 
-import { Link } from "gatsby"
+import {DEVICE_FORM_FACTORS}    from 'statics/strings/reducers/ux';
 
 export default function({
   title,
-  url = "/",
 }) {
   const [hovered, setHovered] = useState(false);
+  const {factor} = useSelector(state => state.root.ux.device);
 
   const handleHover = (e) => {
     e.preventDefault();
     setHovered(!hovered);
   };
 
+  const onMouseOver = (e) => {
+    if (factor !== DEVICE_FORM_FACTORS.MOBILE && factor !== DEVICE_FORM_FACTORS.TABLET) {
+      handleHover(e)
+    }
+  };
+
   return (
-      <div
-          className={style.container}
-      >
+      <div className={style.container}>
           <div
               className={[
                   style.content,
                   ].join(' ')}
           >
-              <Link
-                  to={url}
+              <button
                   className={[
                       hovered && style.hovered
                   ].join(' ')}
-                  onMouseOver = {handleHover}
+                  onMouseOver = {onMouseOver}
                   onClick = {handleHover}
-              >
-                  <div
-                    className={style.cardContent}
-                  >
-                      <div
-                          className={style.infoContainer}
-                      >
-                          {
-                              title ?
-                              <div
-                                  className={style.title}
-                              >
-                                  {title}
-                              </div> : null
-                          }
-                      </div>
-                  </div>
-              </Link>
+              />
           </div>
       </div>
   )

c434d28b9833dab8c76ae89722e7bf17ad6d6fca
Ronan Amsterdam <romanfromrome.com@gmail.com>
2020-01-13 22:43:54 +0800

    because safari aint like the others

--- src/components/squares/index.js
+++ src/components/squares/index.js
@@ -12,7 +12,7 @@ const localeKey = 'squares';
 
 export default function() {
   const min = 1;
-  const [count, setCount] = useState(40);
+  const [count, setCount] = useState(400);
 
   const selectedLocale = useSelector(store => store.root.ux.locale.selected);
   // eslint-disable-next-line
@@ -30,31 +30,31 @@ export default function() {
   return (
     <div className={style.container}>
       <div className={style.content}>
-        <h3>
-          <p>{localize(`${localeKey}.text1`)}</p>
-        </h3>
-        <p>
-          {localize(`${localeKey}.text2`)}
-          {localize(`${localeKey}.text3`)}
-          {localize(`${localeKey}.text4`)}
-        </p>
-        <h1 className={style.fps}><Fps/></h1>
-        <div className={style.inputContainer}>
-          <label htmlFor="count-input"> {localize(`${localeKey}.text5`)} -> </label>
-          <input
-            id="count-input"
-            value={count}
-            type="number"
-            onChange={(e)=> {
-              const newVal = e && e.target && +e.target.value;
-              if (!isNaN(newVal) && newVal > min) {
-                setCount(e.target.value);
-              } else {
-                setCount(min);
-              }
-            }}
-          />
-        </div>
+          <h3>
+            <p>{localize(`${localeKey}.text1`)}</p>
+          </h3>
+          <p>
+            {localize(`${localeKey}.text2`)}
+            {localize(`${localeKey}.text3`)}
+            {localize(`${localeKey}.text4`)}
+          </p>
+          <h1 className={style.fps}><p><Fps/></p></h1>
+          <div className={style.inputContainer}>
+            <label htmlFor="count-input"> {localize(`${localeKey}.text5`)} -> </label>
+            <input
+              id="count-input"
+              value={count}
+              type="number"
+              onChange={(e)=> {
+                const newVal = e && e.target && +e.target.value;
+                if (!isNaN(newVal) && newVal > min) {
+                  setCount(e.target.value);
+                } else {
+                  setCount(min);
+                }
+              }}
+            />
+          </div>
         <div className={style.resultsContainer}>
             <div className={
                 [

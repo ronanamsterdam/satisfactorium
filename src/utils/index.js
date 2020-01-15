03ffbc57932900c64e3b4b8106e5373363bb674e
Roman Zhyliov <romanfromrome.com@gmail.com>
2020-01-15 15:46:55 +0800

    tests initial (#30)

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,17 +2,18 @@
 // you need to call it first on import
 export const debounceCl = _ => (function() {
   let timeout
-
   return function(func, wait = 100, immediate = false) {
     clearTimeout(timeout)
-    timeout = setTimeout(() => {
+    if (immediate) {
       timeout = null
-      if (!immediate) {
-          func.apply(this)
-      }
-    }, wait)
-    if (immediate && !timeout) {
       func.apply(this)
+    } else {
+      timeout = setTimeout(() => {
+        timeout = null
+        if (!immediate) {
+            func.apply(this)
+        }
+      }, wait)
     }
   };
 })();

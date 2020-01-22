c987d0b071cdc027f5f8428fc607067e89722adb
Ronan Amsterdam <romanfromrome.com@gmail.com>
2020-01-22 11:17:24 +0800

    card item upd

--- src/components/shared/links/card/index.js
+++ src/components/shared/links/card/index.js
@@ -3,25 +3,16 @@ import { Link } from "gatsby"
 
 import style from './style.module.less';
 
+export const ActionView = ({isLink, children, ...props}) => {
+  return isLink ? <Link {...props}>{children}</Link> : <button {...props}>{children}</button>
+}
+
 export default function({isLink = true, path, children, subContent, ...props}) {
-  return isLink ?
-      <Link to={"/"+path}
-        className={style.actionItem}
-        {...props}>
-          <div className={style.foreground}>
-            {children}
-            <span className={style.subContent}>{subContent}</span>
-          </div>
-          <div className={style.background} />
-      </Link>
-      :
-      <button
-        className={style.actionItem}
-        {...props}>
-          <div className={style.foreground}>
-            {children}
-            <span className={style.subContent}>{subContent}</span>
-          </div>
-          <div className={style.background} />
-      </button>
+  return <ActionView {...{isLink, to:"/"+path, className: style.actionItem, ...props}}>
+    <div className={style.foreground}>
+      {children}
+      <span className={style.subContent}>{subContent}</span>
+    </div>
+    <div className={style.background} />
+  </ActionView>
 }
